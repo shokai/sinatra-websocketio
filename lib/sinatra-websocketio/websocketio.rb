8f4cd9f164439bbe1e2acdf9a1c97d25b63c63fd
Sho Hashimoto <hashimoto@shokai.org>
2013-03-08 05:51:53 +0900

    continue session_id on re-connect

--- lib/sinatra-websocketio/websocketio.rb
+++ lib/sinatra-websocketio/websocketio.rb
@@ -12,27 +12,25 @@ class WebSocketIO
       end
       puts "WebSocketIO.start port:#{port}"
       EM::WebSocket.run :host => "0.0.0.0", :port => port do |ws|
-        ip_addr = Socket.unpack_sockaddr_in ws.get_peername
-        session_id = create_session ip_addr
         ws.onopen do |handshake|
+          params = parse_handshake_params handshake.path
+          session_id = params[:session] || (create_session Socket.unpack_sockaddr_in ws.get_peername)
           self.sessions[session_id] = {
             :websocket => ws
           }
+          ws.onclose do
+            self.emit :disconnect, session_id
+          end
+          ws.onmessage do |msg|
+            begin
+              data = JSON.parse msg
+            rescue => e
+              self.emit :error, e
+            end
+            self.emit data['type'], data['data'], session_id if data
+          end
           ws.send({:type => :__session_id, :data => session_id}.to_json)
           self.emit :connect, session_id
-        end
-
-        ws.onclose do
-          self.emit :disconnect, session_id
-        end
-        
-        ws.onmessage do |msg|
-          begin
-            data = JSON.parse msg
-          rescue => e
-            self.emit :error, e
-          end
-          self.emit data['type'], data['data'], session_id if data
         end
       end
     end
@@ -54,6 +52,15 @@ class WebSocketIO
 
   def self.create_session(ip_addr)
     Digest::MD5.hexdigest "#{Time.now.to_i}_#{Time.now.usec}_#{ip_addr}"
+  end
+
+  def self.parse_handshake_params(path)
+    tmp = path.gsub(/^\//,'').split(/\=+/)
+    params = {}
+    while !tmp.empty?
+      params[tmp.shift.to_sym] = tmp.shift
+    end
+    params
   end
 
 end
