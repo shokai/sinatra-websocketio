2eca366aa5c370405c9e1e980cf00c6b39b997b7
Sho Hashimoto <hashimoto@shokai.org>
2013-03-09 18:06:36 +0900

    add function EM::WebSocketIO::Client#close  #4

--- lib/em-websocketio-client/client.rb
+++ lib/em-websocketio-client/client.rb
@@ -2,12 +2,15 @@ module EventMachine
   module WebSocketIO
     class Client
       include EventEmitter
-      attr_reader :url, :session
+      attr_reader :url, :session, :running
       attr_accessor :timeout
 
       def initialize(url)
         raise ArgumentError, "invalid websocket URL \"#{url}\"" unless url =~ /^ws:\/\/.+/
         @url = url
+        @running = false
+        @__reconnect_timer_id = nil
+        @ws = nil
         self.on :__session_id do |session_id|
           @session = session_id
           self.emit :connect, session_id
@@ -17,6 +20,7 @@ module EventMachine
       def connect
         url = @session ? "#{@url}/session=#{@session}" : @url
         @ws = EventMachine::WebSocketClient.connect url
+        @running = true
 
         @ws.stream do |msg|
           data = JSON.parse msg
@@ -24,7 +28,7 @@ module EventMachine
         end
 
         @ws.callback do
-          self.push :__session_id, nil
+          self.push :__session_id
         end
 
         @ws.errback do |err|
@@ -33,12 +37,20 @@ module EventMachine
 
         @ws.disconnect do
           self.emit :disconnect
-          EM::add_timer 10 do
-            connect
+          if @running
+            @__reconnect_timer_id = EM::add_timer 10 do
+              connect
+            end
           end
         end
 
         return self
+      end
+
+      def close
+        @running = false
+        @ws.close_connection if @ws
+        EM::cancel_timer @__reconnect_timer_id
       end
 
       def push(type, data={})
