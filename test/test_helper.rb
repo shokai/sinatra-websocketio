c3eab64c7396354e6b52f3d5b7abe87571e1eb01
Sho Hashimoto <hashimoto@shokai.org>
2013-03-16 22:25:57 +0900

    bugfix test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,11 @@
 require 'rubygems'
+require 'bundler/setup'
+Bundler.require
+$:.unshift File.expand_path '../lib', File.dirname(__FILE__)
 require 'minitest/autorun'
 require 'em-websocketio-client'
 require File.expand_path 'app', File.dirname(__FILE__)
 
-$:.unshift File.expand_path '../lib', File.dirname(__FILE__)
 
 ['SIGHUP', 'SIGINT', 'SIGKILL', 'SIGTERM'].each do |sig|
   Kernel.trap sig do
