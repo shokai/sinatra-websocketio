f89a1de9cb25e031701c08b368226feeff4cb0a0
Sho Hashimoto <hashimoto@shokai.org>
2013-03-18 19:26:08 +0900

    require "sinatra/websocketio_nostart"

--- lib/sinatra/websocketio.rb
+++ lib/sinatra/websocketio.rb
@@ -1,19 +1,2 @@
-require 'eventmachine'
-require 'em-websocket'
-require 'json'
-require 'digest/md5'
-require 'event_emitter'
-require 'sinatra/streaming'
-require File.expand_path '../sinatra-websocketio/version', File.dirname(__FILE__)
-require File.expand_path '../sinatra-websocketio/helpers', File.dirname(__FILE__)
-require File.expand_path '../sinatra-websocketio/options', File.dirname(__FILE__)
-require File.expand_path '../sinatra-websocketio/websocketio', File.dirname(__FILE__)
-require File.expand_path '../sinatra-websocketio/javascript', File.dirname(__FILE__)
-require File.expand_path '../sinatra-websocketio/application', File.dirname(__FILE__)
-
-module Sinatra
-  module WebSocketIO
-  end
-  register WebSocketIO
-end
+require File.expand_path 'websocketio_nostart', File.dirname(__FILE__)
 Sinatra::WebSocketIO.start
