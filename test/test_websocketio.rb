c838c06a82ac6db317f421f1fe3847038fddaf6b
Sho Hashimoto <hashimoto@shokai.org>
2013-05-27 04:22:31 +0900

    use MiniTest::Test instead of MiniTest::Unit::TestCase

--- test/test_websocketio.rb
+++ test/test_websocketio.rb
@@ -1,6 +1,6 @@
 require File.expand_path 'test_helper', File.dirname(__FILE__)
 
-class TestWebSocketIO < MiniTest::Unit::TestCase
+class TestWebSocketIO < MiniTest::Test
 
   def setup
     App.start
